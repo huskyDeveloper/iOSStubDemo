06a21ea12278b3b0136b17fe07db6126e795e4d3
Neeraj Mishra <neeraj.mishra01@sap.com>
2017-02-24 18:06:15 +0530

    First commit

---
+++ iOSStubDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSStubDemo
+//
+//  Created by c5249072 on 24/02/17.
+//  Copyright © 2017 Neeraj Mishra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
